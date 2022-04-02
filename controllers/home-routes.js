5aec5bc5835fb23eaff5eae62c591dc943302cd3
Hadasss <hadas.gadish@gmail.com>
2022-04-02 14:02:10 -0700

    user model - deleted email column, commented out password column for testing and seeding purposes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,30 +1,31 @@
 const router = require("express").Router();
 const sequelize = require("../config/connection");
+const { User, Item, Topic } = require("../models");
 
 router.get("/", (req, res) => {
   res.render("homepage");
 });
 
-router.get("/", (req, res) => {
-  Topic.findAll({
-    attributes: ["name"],
-    include: {
-      model: User,
-      attributes: ["username"],
-    },
-  })
-    .then((dbTopicData) => {
-      const topics = dbTopicData.map((topic) => topic.get({ plain: true }));
-      if (!topics) {
-        // TODO add modal "You don't have any topics yet, create your first one!"
-      }
-      res.render("/", { topics, loggedIn: req.session.loggedIn });
-    })
-    .catch((err) => res.status(500).json(err));
-});
+// router.get("/", (req, res) => {
+//   Topic.findAll({
+//     attributes: ["name"],
+//     include: {
+//       model: User,
+//       attributes: ["username"],
+//     },
+//   })
+//     .then((dbTopicData) => {
+//       const topics = dbTopicData.map((topic) => topic.get({ plain: true }));
+//       if (!topics) {
+//         // TODO add modal "You don't have any topics yet, create your first one!"
+//       }
+//       res.render("homepage", { topics, loggedIn: req.session.loggedIn });
+//     })
+//     .catch((err) => res.status(500).json(err));
+// });
 
-router.get("/login", (req, res) => {
-  res.render("login");
-});
+// router.get("/login", (req, res) => {
+//   res.render("login");
+// });
 
 module.exports = router;
