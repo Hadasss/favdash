480eb2e696e20d62eaaaa8bbc63e4fdb1fb7ef8f
David Griffin <david.dtjg2001@gmail.com>
2022-04-04 19:41:26 -0700

    fixed login

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -28,4 +28,22 @@ router.get("/", (req, res) => {
 //   res.render("login");
 // });
 
+router.get('/login', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+
+  res.render('login');
+});
+router.get('/signup', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+
+  res.render('signup');
+});
+
+
 module.exports = router;
